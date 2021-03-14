e50e31e8c0768e52c54d7f091ccbedd68dab2f02
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-14 01:36:08 +0100

    restructuring of connections. added rooms, changed emits

--- server.js
+++ server.js
@@ -11,45 +11,57 @@ const options = {
 };
 const io = require("socket.io")(httpServer, options);
 
-io.on("connect", (socket) => {
+io.on("connection", (socket) => {
   const msgHistory = db.collection("messages");
   //console.log when new User connects
   console.log(`${socket.id} is connected`);
-  msgHistory
-    .find({})
-    .sort({ _id: 1 })
-    .toArray((err, res) => {
-      if (err) {
-        throw err;
-      }
-      socket.emit("history", res);
-      console.log("history emitted");
-    });
-  socket.on("disconnect", (reason) => {
-    console.log(`${socket.id} has disconnected`);
-  });
+  // msgHistory
+  //   .find({})
+  //   .sort({ _id: 1 })
+  //   .toArray((err, res) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     socket.emit("history", res);
+  //     console.log("history emitted");
+  //   });
+  // socket.on("disconnect", (reason) => {
+  //   console.log(`${socket.id} has disconnected`);
+  // });
 
   //emits Chat-History from local MongoDB to User when Room is joined
   socket.on("room", (data) => {
-    console.log("join room");
+    console.log(socket.id + " joined " + data.room);
     console.log(data);
     socket.join(data.room);
-    msgHistory
-      .find({})
-      .sort({ _id: 1 })
-      .toArray((err, res) => {
-        if (err) {
-          throw err;
-        }
-        socket.emit("history", res);
-        console.log("history emitted");
-      });
-  });
 
-  socket.on("leave room", (data) => {
-    console.log("leaving room");
-    console.log(data);
-    socket.leave(data.room);
+    switch (data.room) {
+      case "chat":
+        console.log("chat connected");
+        msgHistory
+          .find({})
+          .sort({ _id: 1 })
+          .toArray((err, res) => {
+            if (err) {
+              throw err;
+            }
+            socket.emit("history", res);
+            console.log("history emitted");
+          });
+        break;
+      case "files":
+        console.log("files connected");
+        break;
+      case "admin":
+        console.log("admin connected");
+        msgHistory.find({}).count((err, res) => {
+          if (err) {
+            throw err;
+          }
+          socket.emit("admin", res);
+          console.log("message count sent");
+        });
+    }
   });
   socket.on("newMessage", (message) => {
     msgHistory.insertOne({
@@ -61,6 +73,28 @@ io.on("connect", (socket) => {
       username: message.username,
     });
   });
+
+  socket.on("leaving room", (data) => {
+    console.log(socket.id + " left " + data.room);
+    socket.leave(data.room);
+  });
+
+  socket.on("admin-stats", () => {
+    const count = io.engine.clientsCount;
+    socket.emit("admin-stats", count);
+    console.log(count);
+  });
+  socket.on("admin", () => {
+    // console.log("admin connected");
+    // msgHistory.find({}).count((err, res) => {
+    //   if (err) {
+    //     throw err;
+    //   }
+    //   socket.emit("admin", res);
+    //   console.log("message count sent");
+    // });
+  });
+
   socket.on("delete", () => {
     msgHistory.deleteMany({});
     setTimeout(() => {
@@ -71,14 +105,15 @@ io.on("connect", (socket) => {
           if (err) {
             throw err;
           }
-          io.emit("history", res);
-          console.log("history emitted");
+          socket.broadcast.to("chat").emit("history", res);
+          console.log("history deleted");
         });
     }, 2000);
   });
   //emits to all sockets when upload-folder gets changed
-  socket.on("files-change", () => {
-    io.emit("loadNewFile");
+  socket.on("files-change", (file) => {
+    socket.in("files").emit("loadNewFile", file);
+    console.log(file);
   });
 });
 
